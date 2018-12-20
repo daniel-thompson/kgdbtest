28fdafe248673935714030fad84a56690976d969
Daniel Thompson <daniel@redfelineninja.org.uk>
2018-12-20 21:11:36 +0000

    interact: Command line processing for graphics mode
    Typically launched with the make wrapper. Try:
    
        make -C ../kgdbtest interact K=gfx
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

--- tests/interact.py
+++ tests/interact.py
@@ -8,11 +8,13 @@ import sys
 def main(argv):
 	kgdb = 'kgdb' in argv[1:]
 	nowait = 'nowait' in argv[1:]
+	gfx = 'gfx' in argv[1:] or 'graphics' in argv[1:]
 
 	kbuild.config(kgdb=True)
 	kbuild.build()
 	ktest.qemu(interactive=True,
 		   gdb=kgdb,
+		   gfx=gfx,
 		   append='' if nowait else 'kgdbwait',
 		   second_uart=kgdb)
 
