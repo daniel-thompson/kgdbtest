f720186a78dfeda41c7558254e66be67adb0535b
Daniel Thompson <daniel@redfelineninja.org.uk>
2018-12-20 21:16:31 +0000

    kgdb_selftest: Split into smaller simpler test cases
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

---
+++ tests/test_kgdb_boottest.py
@@ -0,0 +1,81 @@
+import kbuild
+import ktest
+import pytest
+
+@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm'), run = True,
+		   reason = 'Hangs during concurrency tests')
+@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm64'), run = True,
+		   reason = 'Unexpected kernel single-step exception at EL1')
+def test_kgdbts_boot():
+	kbuild.config(kgdb=True)
+	kbuild.build()
+
+	#
+	# From drivers/misc/kgdbts.c:
+	#
+	# When developing a new kgdb arch specific implementation
+	# or using these tests for the purpose of regression
+	# testing, several invocations are required.
+	#
+	# 1) Boot with the test suite enabled by using the kernel arguments
+	#    "kgdbts=V1F100 kgdbwait"
+	#
+
+	qemu = ktest.qemu(kdb=False, append='kgdbwait kgdbts=V1F100')
+	console = qemu.console
+
+	# Older kernels have no async progress display so for these
+	# kernels we just set a very long timeout (which means failures
+	# will take a long time to report too)
+	if kbuild.get_version() < (4, 17):
+		console.timeout *= 10
+		print('Kernel is below v4.17, extended timeout to {}'.format(
+			console.timeout))
+
+	console.expect('Registered I/O driver kgdbts')
+	console.expect('Entering kdb')
+
+	# Check that the self test starts to run. The failure detect
+	# relies on all error paths calling WARN_ON().
+	choices = ['WARNING.*kgdbts\.c', 'kgdbts:RUN']
+	choice = console.expect(choices)
+	assert choice
+
+	# Ordering is difficult here since the Unregistered I/O... message
+	# is aynschnronous. We solve that just by waiting for all of the
+	# following strings to appear (in any order).
+	choices += [
+		'##INVALID##',
+		'Unregistered I/O driver kgdbts',
+		'Freeing unused kernel.*memory',
+		'Starting logging',
+		'OK',
+		'Welcome to Buildroot',
+		'buildroot login:',
+	]
+
+	num_forks = 0
+	while len(choices) > 3:
+		choice = console.expect(choices)
+		print('Got {} from {}'.format(choice, choices))
+		assert choice
+		if 'login:' in choices[choice]:
+			console.sendline('root')
+			# After logging in then the prompt becomes a
+			# valid choice (before that it's too easy to
+			# mis-match)
+			choices[2] = '#'
+		elif '#' in choices[choice]:
+			# Cause a sys_fork (if there are less than 100 forks
+			# during the boot sequence the self test we started at
+			# boot will not complete)
+			console.sendline('date')
+			num_forks += 1
+			assert num_forks < 100
+
+		if choice >= 3:
+			del choices[choice]
+
+	console.expect_prompt()
+
+	qemu.close()
--- tests/test_kgdb_selftest.py
+++ tests/test_kgdb_selftest.py
@@ -2,27 +2,16 @@ import kbuild
 import ktest
 import pytest
 
-@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm'), run = True,
-		   reason = 'Hangs during concurrency tests')
-@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm64'), run = True,
-		   reason = 'Unexpected kernel single-step exception at EL1')
-def test_selftest():
+@pytest.fixture(scope="module")
+def kernel():
 	kbuild.config(kgdb=True)
 	kbuild.build()
 
-	#
-	# From drivers/misc/kgdbts.c:
-	#
-	# When developing a new kgdb arch specific implementation
-	# or using these tests for the purpose of regression
-	# testing, several invocations are required.
-	#
-	# 1) Boot with the test suite enabled by using the kernel arguments
-	#    "kgdbts=V1F100 kgdbwait"
-	#
+	qemu = ktest.qemu(kdb=False)
 
-	qemu = ktest.qemu(kdb=False, append='kgdbwait kgdbts=V1F100')
 	console = qemu.console
+	console.expect_boot()
+	console.expect_busybox()
 
 	# Older kernels have no async progress display so for these
 	# kernels we just set a very long timeout (which means failures
@@ -32,148 +21,117 @@ def test_selftest():
 		print('Kernel is below v4.17, extended timeout to {}'.format(
 			console.timeout))
 
-	console.expect('Registered I/O driver kgdbts')
-	console.expect('Entering kdb')
+	yield qemu
 
-	# Check that the self test starts to run. The failure detect
-	# relies on all error paths calling WARN_ON().
-	choices = ['WARNING.*kgdbts\.c', 'kgdbts:RUN']
-	choice = console.expect(choices)
-	assert choices
+	qemu.close()
 
-	# Ordering is difficult here since the Unregistered I/O... message
-	# is aynschnronous. We solve that just by waiting for all of the
-	# following strings to appear (in any order).
-	choices += [
-		'##INVALID##',
-		'Unregistered I/O driver kgdbts',
-		'Freeing unused kernel memory',
-		'Starting logging',
-		'OK',
-		'Welcome to Buildroot',
-		'buildroot login:',
-	]
+#@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm'), run = True,
+#		   reason = 'Hangs during concurrency tests')
 
-	num_forks = 0
-	while len(choices) > 3:
-		print(choices)
-		choice = console.expect(choices)
-		assert choices
-		if 'login:' in choices[choice]:
-			console.sendline('root')
-			# After logging in then the prompt becomes a
-			# valid choice (before that it's too easy to
-			# mis-match)
-			choices[2] = '#'
-		elif '#' in choices[choice]:
-			# Cause a sys_fork (if there are less than 100 forks
-			# during the boot sequence the self test we started at
-			# boot will not complete)
-			console.sendline('date')
-			num_forks += 1
-			assert num_forks < 100
+def test_kgdbts_V1(kernel):
+	'''
+	From drivers/misc/kgdbts.c:
 
-		if choice >= 3:
-			del choices[choice]
-
-	console.expect_prompt()
-
-	#
-	# From drivers/misc/kgdbts.c:
-	#
-	# 2) After the system boot run the basic test.
-	#    echo kgdbts=V1 > /sys/module/kgdbts/parameters/kgdbts
-	#
-
-	console.sendline('echo kgdbts=V1 > /sys/module/kgdbts/parameters/kgdbts')
-	choice = console.expect(['ERROR', 'Registered I/O driver kgdbts'])
+	2) After the system boot run the basic test.
+	   echo kgdbts=V1 > /sys/module/kgdbts/parameters/kgdbts
+	'''
+	kernel.console.sendline('echo kgdbts=V1 > /sys/module/kgdbts/parameters/kgdbts')
+	choice = kernel.console.expect(['ERROR', 'Registered I/O driver kgdbts'])
 	assert choice
 	choices = [ 'WARNING.*kgdbts\.c', 'kgdbts:RUN',
 			'Unregistered I/O driver kgdbts']
 	choice = 0
 	while choice != 2:
-		choice = console.expect(choices)
-		assert choices
+		choice = kernel.console.expect(choices)
+		assert choice
 
-	console.expect_prompt()
+	kernel.console.expect_prompt()
 
-	#
-	# From drivers/misc/kgdbts.c:
-	#
-	# 3) Run the concurrency tests.  It is best to use n+1
-	#    while loops where n is the number of cpus you have
-	#    in your system.  The example below uses only two
-	#    loops.
-	#
-	#    ## This tests break points on sys_open
-	#    while [ 1 ] ; do find / > /dev/null 2>&1 ; done &
-	#    while [ 1 ] ; do find / > /dev/null 2>&1 ; done &
-	#    echo kgdbts=V1S10000 > /sys/module/kgdbts/parameters/kgdbts
-	#    fg # and hit control-c
-	#    fg # and hit control-c
-	#
+def test_kgdbts_V1S10000(kernel):
+	'''
+	From drivers/misc/kgdbts.c:
 
-	console.sendline('n=$((`nproc` + 1))')
-	console.sendline('for i in `seq $n`')
-	console.sendline('do')
-	console.sendline('while [ 1 ] ; do find / > /dev/null 2>&1 ; done &')
-	console.sendline('done')
-	console.expect_prompt()
-	console.sendline('jobs | tee j')
-	console.expect_prompt()
-	console.sendline('[ `cat j | wc -l` -eq $n ] && [ $n -gt 1 ] && echo OK || echo FAILED')
-	choice = console.expect(['FAILED', 'OK'])
+	3) Run the concurrency tests.  It is best to use n+1
+	   while loops where n is the number of cpus you have
+	   in your system.  The example below uses only two
+	   loops.
+
+	   ## This tests break points on sys_open
+	   while [ 1 ] ; do find / > /dev/null 2>&1 ; done &
+	   while [ 1 ] ; do find / > /dev/null 2>&1 ; done &
+	   echo kgdbts=V1S10000 > /sys/module/kgdbts/parameters/kgdbts
+	   fg # and hit control-c
+	   fg # and hit control-c
+	'''
+	kernel.console.sendline('n=$((`nproc` + 1))')
+	kernel.console.sendline('for i in `seq $n`')
+	kernel.console.sendline('do')
+	kernel.console.sendline('while [ 1 ] ; do find / > /dev/null 2>&1 ; done &')
+	kernel.console.sendline('done')
+	kernel.console.expect_prompt()
+	kernel.console.sendline('jobs | tee j')
+	kernel.console.expect_prompt()
+	kernel.console.sendline('[ `cat j | wc -l` -eq $n ] && [ $n -gt 1 ] && echo OK || echo FAILED')
+	choice = kernel.console.expect(['FAILED', 'OK'])
 	assert choice
-	console.expect_prompt()
+	kernel.console.expect_prompt()
 
-	console.sendline('echo kgdbts=V1S10000 > /sys/module/kgdbts/parameters/kgdbts')
-	#console.sendline('echo kgdbts=V1S1000 > /sys/module/kgdbts/parameters/kgdbts')
+	kernel.console.sendline('echo kgdbts=V1S10000 > /sys/module/kgdbts/parameters/kgdbts')
+	#kernel.console.sendline('echo kgdbts=V1S1000 > /sys/module/kgdbts/parameters/kgdbts')
+	choice = kernel.console.expect(['ERROR', 'Registered I/O driver kgdbts'])
+	assert choice
 
+	choices = [ 'WARNING.*kgdbts\.c', 'kgdbts:RUN',
+			'Unregistered I/O driver kgdbts']
 	choice = 0
 	while choice != 2:
-		choice = console.expect(choices)
-		assert choices
+		choice = kernel.console.expect(choices)
+		assert choice
 
 	# TODO: We'd like to expect_prompt() here but until it has resync
 	#       support its too risky!
 
-	console.sendline('for i in `seq $n`; do kill %$i; done')
-	console.expect_prompt()
+	kernel.console.sendline('for i in `seq $n`; do kill %$i; done')
+	kernel.console.expect_prompt()
 
-	#
-	# From drivers/misc/kgdbts.c:
-	#
-	#    ## This tests break points on do_fork
-	#    while [ 1 ] ; do date > /dev/null ; done &
-	#    while [ 1 ] ; do date > /dev/null ; done &
-	#    echo kgdbts=V1F1000 > /sys/module/kgdbts/parameters/kgdbts
-	#   fg # and hit control-c
-	#
+@pytest.mark.xfail(condition = (kbuild.get_arch() == 'arm64'), run = True,
+		   reason = 'Unexpected kernel single-step exception at EL1')
+def test_kgdbts_V1F1000(kernel):
+	'''
+	From drivers/misc/kgdbts.c:
 
-	console.sendline('n=$((`nproc` + 1))')
-	console.sendline('for i in `seq $n`')
-	console.sendline('do')
-	console.sendline('while [ 1 ] ; do date > /dev/null 2>&1 ; done &')
-	console.sendline('done')
-	console.expect_prompt()
-	console.sendline('jobs | tee j')
-	console.expect_prompt()
-	console.sendline('[ `cat j | wc -l` -eq $n ] && [ $n -gt 1 ] && echo OK || echo FAILED')
-	choice = console.expect(['FAILED', 'OK'])
+	   ## This tests break points on do_fork
+	   while [ 1 ] ; do date > /dev/null ; done &
+	   while [ 1 ] ; do date > /dev/null ; done &
+	   echo kgdbts=V1F1000 > /sys/module/kgdbts/parameters/kgdbts
+	   fg # and hit control-c
+	'''
+	kernel.console.sendline('n=$((`nproc` + 1))')
+	kernel.console.sendline('for i in `seq $n`')
+	kernel.console.sendline('do')
+	kernel.console.sendline('while [ 1 ] ; do date > /dev/null 2>&1 ; done &')
+	kernel.console.sendline('done')
+	kernel.console.expect_prompt()
+	kernel.console.sendline('jobs | tee j')
+	kernel.console.expect_prompt()
+	kernel.console.sendline('[ `cat j | wc -l` -eq $n ] && [ $n -gt 1 ] && echo OK || echo FAILED')
+	choice = kernel.console.expect(['FAILED', 'OK'])
 	assert choice
-	console.expect_prompt()
+	kernel.console.expect_prompt()
 
-	console.sendline('echo kgdbts=V1F1000 > /sys/module/kgdbts/parameters/kgdbts')
+	kernel.console.sendline('echo kgdbts=V1F1000 > /sys/module/kgdbts/parameters/kgdbts')
+	choice = kernel.console.expect(['ERROR', 'Registered I/O driver kgdbts'])
+	assert choice
 
+	choices = [ 'WARNING.*kgdbts\.c', 'kgdbts:RUN',
+			'Unregistered I/O driver kgdbts']
 	choice = 0
 	while choice != 2:
-		choice = console.expect(choices)
-		assert choices
+		choice = kernel.console.expect(choices)
+		assert choice
 
 	# TODO: We'd like to expect_prompt() here but until it has resync
 	#       support its too risky!
 
-	console.sendline('for i in `seq $n`; do kill %$i; done')
-	console.expect_prompt()
-
-	qemu.close()
+	kernel.console.sendline('for i in `seq $n`; do kill %$i; done')
+	kernel.console.expect_prompt()
