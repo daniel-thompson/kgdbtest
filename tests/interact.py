88cb593d9f7f27e8cb2bf9713cede0a57ad1697b
Daniel Thompson <daniel.thompson@linaro.org>
2020-06-17 11:58:22 +0100

    interact: Allow K= to be used to configure the kernel commandline
    A simple example:
    
        make interact K="nowait earlycon"
    
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- tests/interact.py
+++ tests/interact.py
@@ -10,12 +10,18 @@ def main(argv):
 	nowait = 'nowait' in argv[1:]
 	gfx = 'gfx' in argv[1:] or 'graphics' in argv[1:]
 
+	args = [ arg for arg in argv[1:] if arg not in
+		('kgdb', 'nowait', 'gfx', 'graphics') ]
+
+	if not nowait:
+		args.append('kgdbwait')
+
 	kbuild.config(kgdb=True)
 	kbuild.build()
 	ktest.qemu(interactive=True,
 		   gdb=kgdb,
 		   gfx=gfx,
-		   append='' if nowait else 'kgdbwait',
+		   append=' '.join(args),
 		   second_uart=kgdb)
 
 if __name__ == '__main__':
